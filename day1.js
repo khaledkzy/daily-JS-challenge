bfdc67aba00c7711d4f2a976778d37739aa5c69f
khaled <khaled.kzy@gmail.com>
2017-06-07 11:56:07 +0100

    Daily Challenge Day 1

---
+++ day1.js
@@ -0,0 +1,83 @@
+
+// // Christmass Tree 1 Challenge
+
+// var str="";
+//     for(var i=1; i <=10; i++){
+//         str += "*";
+//         console.log(str);
+
+//     }
+
+
+ // Christmass Tree 2 Challenge   
+
+
+// var counter = 0;
+// for (var i = 10; i > 0; i--) {
+// 	counter++;
+// 	var result = "";
+// 	for (var j = 0; j < i; j++) {
+// 		result += " ";
+// 	}
+// 	for (var k = counter; k > 0; k--) {
+// 		result += "*";
+// 	}
+// 	console.log(result);}
+//*****************************************************
+
+//     for (var numberOfStars = 1; numberOfStars <= 10; numberOfStars++) {
+// 	var result = "";
+//     for(var spaces = 10; spaces>0; spaces--) {
+//         if(spaces > numberOfStars) {
+//             result += " ";
+//         } else {
+//             result += "*";
+//         }
+//     }
+//     console.log(result);
+// }
+//*****************************************************
+
+// var stars = "";
+// for (var j = 0, k = 10 ; j <= 10, k >= 0 ; j++, k--) {
+//     empty = Array( k + 1 ).join(' ');
+//     console.log(empty + stars);
+//     stars += "*";
+// }
+//*****************************************************
+
+// Christmass Tree 3 Challenge  
+// var stars = "*";
+// for (k = 10 ; k >= 0 ; k--) {
+//     var empty = Array( k + 1 ).join(' ');
+//     console.log(empty + stars);
+//     stars += "**";
+// }
+// console.log("         ***");
+// console.log("         ***");
+
+//*****************************************************
+// var counter = 1;
+// for (var i = 12; i > 0; i--) {
+// 	counter = counter + 2;
+// 	var result = "";
+// 	if (i > 2) {
+// 		for (var j = 0; j < i; j++) {
+// 			result += " ";
+// 		}
+// 		for (var k = counter - 2; k > 0; k--) {
+// 			result += "*";
+// 		}
+// 		console.log(result);
+// 	}
+// 	else {
+// 		for (var j = 11; j > 0; j--) {
+// 			result += " ";
+// 		}
+// 		for (var k = 5; k > 2; k--) {
+// 			result += "*";
+// 		}
+// 		console.log(result);
+// 	}
+// }
+
