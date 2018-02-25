386e62cf7b33e774922c83e98a183dd6f1b16eb8
khaledkzy <khaledkzy@gmail.com>
2018-02-25 12:03:53 +0000

    Solve with an alternative way and less code

--- Day 1/Solutions/day1.js
+++ Day 1/Solutions/day1.js
@@ -31,3 +31,22 @@ for (k = 10 ; k >= 0 ; k--) {
 
 console.log("         ***");
 console.log("         ***");
+
+// ____________________________________________________ My Answers ____________________________________________________//
+// Challange 1 - Print a left tree ( khaledkzy)
+var str = "*";
+for (var i = 1; i <= 10; i++) { console.log(str.repeat(i)) }
+
+// Challange 2 - Print a right tree ( khaledkzy )
+
+var star = "*"
+var empty = " "
+for (var i = 10, j = 1; i > 0, j < 10; i-- , j++) { console.log(empty.repeat(i) + star.repeat(j)); }
+
+// Challenge 3 ( khaledkzy )
+var star = "*"
+var empty = " "
+for (var i = 10, j = 1; i > 0, j < 10; i-- , j++) {
+    j === 1 ? console.log(empty.repeat(i) + star.repeat(j)) : console.log(empty.repeat(i) + star.repeat(j) + star.repeat(j));
+}
+console.log("         ***\n         ***");
